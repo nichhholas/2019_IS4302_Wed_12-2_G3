bfe0752920aa3883b4b034a7d43cf4e66f53eee7
quahjingwen <quahjingwen@yahoo.com.sg>
2019-04-02 21:14:55 +0800

    donor page

--- src/components/Donor.vue
+++ src/components/Donor.vue
@@ -1,6 +1,7 @@
 <template>
   <div>
     <h2>Donor Page</h2>
+    <p> Thank you for registering with us. Please refer to below for more information on our fundraising event and how you can donate. </p>
     <div class="event">
     <h3><strong>Current Event</strong></h3>
     <img src="../assets/NKF.jpg" alt ="photo of NKF">
@@ -18,81 +19,189 @@
     <h4>Record Your Donation</h4>
     <input v-model="amount">
     <button @click="makeDonation(amount)"> Donate </button><br><br>
-    <h4>My Donations</h4> 
-    <!-- {{ownDonations}} -->
-    <button @click="fetchOwnDonations"> Show Donations </button><br>
-    <table id="firstTable" class="center" v-if="showOwnDonations">
-      <thead>
-        <tr>
-          <th>Type</th>
-          <th>DocumentID</th>
-          <th>From</th>
-          <th>Treasury</th>
-          <th>Amount</th>
-          <th>Status</th>
-        </tr>
-      </thead>
-      <tbody>
-        <tr v-for="row in this.ownDonations" :key="row.transactionId">
-          <td>{{row.type}}</td>
-          <td>{{row.documentID}}</td>
-          <td>{{row.creator}}</td>
-          <td>{{row.treasury}}</td>
-          <td>{{row.amount}}</td>
-          <td>{{row.status}}</td>
-        </tr>
-      </tbody>
-    </table>
-    <br>
-    <h4>All Rejected Donations</h4>
-    <button @click="fetchRejected()"> Show Rejected Donations </button><br>
-    <table id="firstTable" class="center" v-if="showRejectedDonations">
-      <thead>
-        <tr>
-          <th>Type</th>
-          <th>DocumentID</th>
-          <th>From</th>
-          <th>Treasury</th>
-          <th>Amount</th>
-          <th>Status</th>
-        </tr>
-      </thead>
-      <tbody>
-        <tr v-for="row in this.rejectedDonations" :key="row.transactionId">
-          <td>{{row.type}}</td>
-          <td>{{row.documentID}}</td>
-          <td>{{row.creator}}</td>
-          <td>{{row.treasury}}</td>
-          <td>{{row.amount}}</td>
-          <td>{{row.status}}</td>
-        </tr>
-      </tbody>
-    </table>
-    <br><br>
 
-    <h4>All Transactions</h4>
-    <button @click="fetchDistributions"> Show Transactions </button><br>
-    <table id="allTxn" class="allTxn">
-      <thead>
-        <tr>
-          <th>DocumentID</th>
-          <th>To</th>
-          <th>Treasury</th>
-          <th>Amount</th>
-          <th>Status</th>
-        </tr>
-      </thead>
-      <tbody>
-        <tr v-for="row in this.distributions" :key="row.transactionId">
-          <td>{{row.documentID}}</td>
-          <td>{{row.beneficiary}}</td>
-          <td>{{row.treasury}}</td>
-          <td>{{row.amount}}</td>
-          <td>{{row.status}}</td>
-        </tr>
-      </tbody>
-    </table>
-    <br>
+    <!-- start of collapsible -->
+    <div role="tablist">
+    <b-card no-body class="mb-1">
+      <b-card-header header-tag="header" class="p-1" role="tab">
+        <b-button href="#" v-b-toggle.accordion-1 variant="info">My Donations</b-button>
+      </b-card-header>
+      <b-collapse id="accordion-1" accordion="my-accordion" role="tabpanel">
+        <b-card-body>
+
+          <!-- Donation Records Collapsible Buttons -->
+        <b-button v-b-toggle.collapse-1-inner class="m-1">Pending</b-button>
+        <b-button v-b-toggle.collapse-2-inner class="m-1">Confirmed</b-button>
+        <b-button v-b-toggle.collapse-3-inner class="m-1">Rejected</b-button>
+
+        <b-collapse id="collapse-1-inner" class="mt-2">
+            <b-card-text>
+                <u><h4> Pending Donations </h4></u>
+                <table id="firstTable" class="center">
+                <thead>
+                  <tr>
+                    <th>Type</th>
+                    <th>DocumentID</th>
+                    <th>From</th>
+                    <th>Treasury</th>
+                    <th>Amount</th>
+                    <th>Status</th>
+                  </tr>
+                </thead>
+                <tbody>
+                  <tr v-for="row in this.pendingDonations" :key="row.transactionId">
+                    <td>{{row.type}}</td>
+                    <td>{{row.documentID}}</td>
+                    <td>{{row.creator}}</td>
+                    <td>{{row.treasury}}</td>
+                    <td>{{row.amount}}</td>
+                    <td>{{row.status}}</td>
+                  </tr>
+                </tbody>
+              </table>
+            </b-card-text>
+        </b-collapse>
+
+        <b-collapse id="collapse-2-inner" class="mt-2">
+            <b-card-text>
+                <u><h4> Confirmed Donations </h4></u>
+                <table id="firstTable" class="center">
+                <thead>
+                  <tr>
+                    <th>Type</th>
+                    <th>DocumentID</th>
+                    <th>From</th>
+                    <th>Treasury</th>
+                    <th>Amount</th>
+                    <th>Status</th>
+                  </tr>
+                </thead>
+                <tbody>
+                  <tr v-for="row in this.ownDonations" :key="row.transactionId">
+                    <td>{{row.type}}</td>
+                    <td>{{row.documentID}}</td>
+                    <td>{{row.creator}}</td>
+                    <td>{{row.treasury}}</td>
+                    <td>{{row.amount}}</td>
+                    <td>{{row.status}}</td>
+                  </tr>
+                </tbody>
+              </table>
+            </b-card-text>
+        </b-collapse>
+
+        <b-collapse id="collapse-3-inner" class="mt-2">
+            <b-card-text>
+                <u><h4> Rejected Donations </h4></u>
+                <table id="firstTable" class="center">
+                <thead>
+                  <tr>
+                    <th>Type</th>
+                    <th>DocumentID</th>
+                    <th>From</th>
+                    <th>Treasury</th>
+                    <th>Amount</th>
+                    <th>Status</th>
+                  </tr>
+                </thead>
+                <tbody>
+                  <tr v-for="row in this.rejectedDonations" :key="row.transactionId">
+                    <td>{{row.type}}</td>
+                    <td>{{row.documentID}}</td>
+                    <td>{{row.creator}}</td>
+                    <td>{{row.treasury}}</td>
+                    <td>{{row.amount}}</td>
+                    <td>{{row.status}}</td>
+                  </tr>
+                </tbody>
+              </table>
+            </b-card-text>
+        </b-collapse>
+
+          </b-card-body>
+      </b-collapse>
+    </b-card>
+
+    <b-card no-body class="mb-1">
+      <b-card-header header-tag="header" class="p-1" role="tab">
+        <b-button href="#" v-b-toggle.accordion-2 variant="info">All Blockchain Transactions</b-button>
+      </b-card-header>
+      <b-collapse id="accordion-2" accordion="my-accordion" role="tabpanel">
+        <b-card-body>
+
+          <!--Blockchain Transactions Collapsible Buttons -->
+        <b-button v-b-toggle.collapse-1-inner class="m-1">Financial Transactions</b-button>
+        <b-button v-b-toggle.collapse-2-inner class="m-1">Bank Transactions</b-button>
+
+        <b-collapse id="collapse-1-inner" class="mt-2">
+            <b-card-text>
+              <u><h4>Financial Transactions</h4></u>
+              <p>Current status of Donations by Donors and Withdrawals by Charity Organisations, that is verified by the Treasury</p>
+              <p>'Pending' shows Donations/Withdrawals that has been identified by the Treasury</p>
+              <p>'Successful' and 'Denied' shows Donations/Withdrawals that has been verified by the Treasury</p>
+                <table id="firstTable" class="center">
+                  <thead>
+                    <tr>
+                      <th>Type</th>
+                      <th>DocumentID</th>
+                      <th>From</th>
+                      <th>Treasury</th>
+                      <th>Amount</th>
+                      <th>Status</th>
+                    </tr>
+                  </thead>
+                  <tbody>
+                    <tr v-for="row in this.financialRecord" :key="row.transactionId">
+                      <td>{{row.type}}</td>
+                      <td>{{row.documentID}}</td>
+                      <td>{{row.creator}}</td>
+                      <td>{{row.treasury}}</td>
+                      <td>{{row.amount}}</td>
+                      <td>{{row.status}}</td>
+                    </tr>
+                  </tbody>
+                </table>
+            </b-card-text>
+        </b-collapse>
+
+        <b-collapse id="collapse-2-inner" class="mt-2">
+            <b-card-text>
+            <u><h4>Bank Transactions</h4></u>
+            <p>Current status of Distributions to Beneficiaries by Charity Organisations</p> 
+            <p>'Pending' shows Distributions that has been identified by the Treasury</p>
+            <p>'Successful' and 'Denied' shows Distributions that has been verified by the Treasury</p>
+                <table id="firstTable" class="center">
+                  <thead>
+                    <tr>
+                      <th>DocumentID</th>
+                      <th>To</th>
+                      <th>Treasury</th>
+                      <th>Amount</th>
+                      <th>Status</th>
+                    </tr>
+                  </thead>
+                  <tbody>
+                    <tr v-for="row in this.bankStatements" :key="row.transactionId">
+                      <td>{{row.documentID}}</td>
+                      <td>{{row.beneficiary}}</td>
+                      <td>{{row.treasury}}</td>
+                      <td>{{row.amount}}</td>
+                      <td>{{row.status}}</td>
+                    </tr>
+                  </tbody>
+                </table>
+            </b-card-text>
+        </b-collapse>
+
+          </b-card-body>
+      </b-collapse>
+    </b-card>
+
+  </div>
+
+  <!-- end of collapsible -->
+
+    <br><br>
 
     <div class="wrapper"><br>
       <!-- <button>Donate</button><br><br> -->
@@ -111,11 +220,14 @@ export default {
         id: this.$route.params.id,
         ownDonations: null,
         showOwnDonations: false,
+        rejectedDonations: [],
+        pendingDonations:[],
         max: 10000,
         value: 750,
         amount: 0,
         treasury: null,
-      distributions: null,
+      bankStatements: null,
+      financialRecord:null,
       myDonation: [
         { date: "1/1/19", Amount: 100 },
         { date: "1/2/19", Amount: 100 }
@@ -124,6 +236,11 @@ export default {
   },
   mounted(){
     this.fetchTreasury();
+    this.fetchOwnDonations();
+    this.fetchRejected();
+    this.fetchBankStatements();
+    this.fetchPendingDonations();
+    this.fetchFinancialRecord();
   },
   methods:{
     makeDonation(amount){
@@ -138,12 +255,21 @@ export default {
             alert("An error occurred, your donation was not recorded");
         });
     },
-    async fetchDistributions(){
-      console.log("fetch distributions");
-      await fetch("http://localhost:3002/api/org.acme.charity.BankStatement?filter=%7B%22where%22%3A%7B%22status%22%3A%22Successful%22%7D%7D")
+    async fetchBankStatements(){
+      console.log("fetch bank statements");
+      await fetch("http://localhost:3003/api/org.acme.charity.BankStatement")
       .then(response => response.json())
       .then((data)=>(
-          this.distributions = data
+          this.bankStatements = data
+          ))
+      this.showTxn();
+    },
+    async fetchFinancialRecord(){
+      console.log("fetch bank statements");
+      await fetch("http://localhost:3003/api/org.acme.charity.FinancialRecord")
+      .then(response => response.json())
+      .then((data)=>(
+          this.financialRecord = data
           ))
       this.showTxn();
     },
@@ -163,7 +289,14 @@ export default {
       .then((data)=>(
           this.rejectedDonations = data
           ))
-        this.showRejectedDonations = true;
+    },
+    async fetchPendingDonations(){
+      var url = "http://localhost:3001/api/org.acme.charity.FinancialRecord?filter=%7B%22where%22%3A%7B%22status%22%3A%22Pending%22%2C%20%22creator%22%3A%22resource%3Aorg.acme.charity.Donor%23"+this.id+"%22%7D%7D"
+      await fetch(url)
+      .then(response => response.json())
+      .then((data)=>(
+          this.pendingDonations = data
+          ))
     },
     showDonation: function(){
       document.getElementById("donationTable").style.display="inline-table";
