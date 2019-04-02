a812df2a63e9d8ed08a3fcf856acbff0c36e7c1e
quahjingwen <quahjingwen@yahoo.com.sg>
2019-04-02 18:52:29 +0800

    treasury page

--- src/components/Treasury.vue
+++ src/components/Treasury.vue
@@ -2,7 +2,8 @@
 <template>
     <div id='App'>
         <h1>Verify Records Page</h1>
-        <h3> Treasury Info</h3>
+
+    <h2> Treasury Info</h2>
         <table id="firstTable" class="center">
         <thead>
             <tr>
@@ -17,8 +18,25 @@
             </tr>
         </tbody>
         </table>
+        <br>
 
-        <h3> Donation Records </h3>
+    <!-- start of collapsible -->
+    <div role="tablist">
+    <b-card no-body class="mb-1">
+      <b-card-header header-tag="header" class="p-1" role="tab">
+        <b-button href="#" v-b-toggle.accordion-2 variant="info">Pending Transactions</b-button>
+      </b-card-header>
+      <b-collapse id="accordion-2" accordion="my-accordion" role="tabpanel">
+        <b-card-body>
+            
+        <!-- Pending Records Collapsible Buttons -->
+        <b-button v-b-toggle.collapse-1-inner class="m-1">Pending Donation Records</b-button>
+        <b-button v-b-toggle.collapse-2-inner class="m-1">Pending Withdrawal Records</b-button>
+        <b-button v-b-toggle.collapse-3-inner class="m-1">Pending Bank Statements</b-button>
+
+        <!-- Pending Records Collapsible Informations -->
+        <b-collapse id="collapse-1-inner" class="mt-2">
+        <u><h4> Pending Donation Records </h4></u>
         <table id="firstTable" class="center">
         <thead>
             <tr>
@@ -30,7 +48,7 @@
             </tr>
         </thead>
         <tbody>
-            <tr v-for="row in donationRecords" :key="row.documentID">
+            <tr v-for="row in pendingDonationRecords" :key="row.documentID">
             <td>{{row.documentID}}</td>
             <td>{{row.type}}</td>
             <td>{{row.amount}}</td>
@@ -42,8 +60,12 @@
             </tr>
         </tbody>
         </table>
+          </b-card-text>
+        </b-collapse>
 
-        <h3> Withdrawal Records </h3>
+        <b-collapse id="collapse-2-inner" class="mt-2">
+        <b-card-text>
+        <u><h4> Pending Withdrawal Records </h4></u>
         <table id="firstTable" class="center">
         <thead>
             <tr>
@@ -55,7 +77,7 @@
             </tr>
         </thead>
         <tbody>
-            <tr v-for="row in withdrawalRecords" :key="row.documentID">
+            <tr v-for="row in pendingWithdrawalRecords" :key="row.documentID">
             <td>{{row.documentID}}</td>
             <td>{{row.type}}</td>
             <td>{{row.amount}}</td>
@@ -67,8 +89,12 @@
             </tr>
         </tbody>
         </table>
+        </b-card-text>
+        </b-collapse>
 
-        <h3> Bank Statements </h3>
+        <b-collapse id="collapse-3-inner" class="mt-2">
+        <b-card-text>
+        <u><h4> Pending Bank Statements </h4></u>
         <table id="firstTable" class="center">
         <thead>
             <tr>
@@ -80,7 +106,7 @@
             </tr>
         </thead>
         <tbody>
-            <tr v-for="row in bankStatements" :key="row.documentID">
+            <tr v-for="row in pendingBankStatements" :key="row.documentID">
             <td>{{row.documentID}}</td>
             <td>{{row.beneficiary}}</td>
             <td>{{row.amount}}</td>
@@ -92,6 +118,106 @@
             </tr>
         </tbody>
         </table>
+        <br></b-card-text>
+        </b-collapse>
+
+        </b-card-body>
+      </b-collapse>
+    </b-card>
+
+    <b-card no-body class="mb-1">
+      <b-card-header header-tag="header" class="p-1" role="tab">
+        <b-button href="#" v-b-toggle.accordion-3 variant="info">Confirmed Transactions</b-button>
+      </b-card-header>
+      <b-collapse id="accordion-3" accordion="my-accordion" role="tabpanel">
+        <b-card-body>
+
+            <!-- Pending Records Collapsible Buttons -->
+            <b-button v-b-toggle.collapse-1-inner class="m-1">Confirmed Donation Records</b-button>
+            <b-button v-b-toggle.collapse-2-inner class="m-1">Confirmed Withdrawal Records</b-button>
+            <b-button v-b-toggle.collapse-3-inner class="m-1">Confirmed Bank Statements</b-button>
+
+            <b-collapse id="collapse-1-inner" class="mt-2">
+                <b-card-text>
+        <u><h4> Confirmed Donation Records </h4></u>
+        <table id="firstTable" class="center">
+        <thead>
+            <tr>
+            <th>Document ID</th>
+            <th>Type</th>
+            <th>Amount</th>
+            <th>Status</th>
+            </tr>
+        </thead>
+        <tbody>
+            <tr v-for="row in filteredConfirmedDonationRecords" :key="row.documentID">
+            <td>{{row.documentID}}</td>
+            <td>{{row.type}}</td>
+            <td>{{row.amount}}</td>
+            <td>{{row.status}}</td>
+            </tr>
+        </tbody>
+        </table>
+        </b-card-text>
+            </b-collapse>
+
+            <b-collapse id="collapse-2-inner" class="mt-2">
+                <b-card-text>
+        <u><h4> Confirmed Withdrawal Records </h4></u>
+        <table id="firstTable" class="center">
+        <thead>
+            <tr>
+            <th>Document ID</th>
+            <th>Type</th>
+            <th>Amount</th>
+            <th>Status</th>
+            </tr>
+        </thead>
+        <tbody>
+            <tr v-for="row in filteredConfirmedWithdrawalRecords" :key="row.documentID">
+            <td>{{row.documentID}}</td>
+            <td>{{row.type}}</td>
+            <td>{{row.amount}}</td>
+            <td>{{row.status}}</td>
+            </tr>
+        </tbody>
+        </table>
+        </b-card-text>
+            </b-collapse>
+
+            <b-collapse id="collapse-3-inner" class="mt-2">
+                <b-card-text>
+        <u><h4> Confirmed Bank Statements </h4></u>
+        <table id="firstTable" class="center">
+        <thead>
+            <tr>
+            <th>Document ID</th>
+            <th>Beneficiary</th>
+            <th>Amount</th>
+            <th>Status</th>
+            </tr>
+        </thead>
+        <tbody>
+            <tr v-for="row in confirmedBankStatements" :key="row.documentID">
+            <td>{{row.documentID}}</td>
+            <td>{{row.beneficiary}}</td>
+            <td>{{row.amount}}</td>
+            <td>{{row.status}}</td>
+            </tr>
+        </tbody>
+        </table>
+          </b-card-text>
+            </b-collapse>
+
+
+        </b-card-body>
+      </b-collapse>
+    </b-card>
+  </div>
+
+  <!-- end of collapsible -->
+
+
         <br><br>
             <button @click="logout" >Log Out</button>
 
@@ -106,9 +232,11 @@ import router from '@/router/index.js'
 export default {
     data() {
         return {
-            donationRecords:{},
-            withdrawalRecords:{},
-            bankStatements:null,
+            pendingDonationRecords:{},
+            confirmedFinancialRecords:[],
+            pendingWithdrawalRecords:[],
+            pendingBankStatements:{},
+            confirmedBankStatements:[],
             treasury: null
         }
     },
@@ -140,31 +268,49 @@ export default {
             alert("An error occurred, your distribution was not recorded");
         });
     },
-    async fetchDonationRecords(){
+    async fetchPendingDonationRecords(){
       console.log("fetch donation records");
-      await fetch("http://localhost:3003/api/org.acme.charity.FinancialRecord?filter=%7B%22where%22%3A%7B%22type%22%3A%22Donation%22%7D%7D")
+      await fetch("http://localhost:3003/api/org.acme.charity.FinancialRecord?filter=%7B%22where%22%3A%7B%22status%22%3A%22Pending%22%2C%22type%22%3A%22Donation%22%7D%7D")
       .then(response => response.json())
       .then((data)=>(
-          this.donationRecords = data
+          this.pendingDonationRecords = data
           ))
     },
-    async fetchWithdrawalRecords(){
-      console.log("fetch withdrawal records");
-      await fetch("http://localhost:3003/api/org.acme.charity.FinancialRecord?filter=%7B%22where%22%3A%7B%22type%22%3A%22Withdrawal%22%7D%7D")
+    async fetchConfirmedFinancialRecords(){
+      console.log("fetch confirmed financial records");
+      await fetch("http://localhost:3003/api/org.acme.charity.FinancialRecord?filter=%7B%22where%22%3A%7B%22or%22%3A%5B%7B%22status%22%3A%22Denied%22%7D%2C%7B%22status%22%3A%22Successful%22%7D%5D%7D%7D")
       .then(response => response.json())
       .then((data)=>(
-          this.withdrawalRecords = data
+          this.confirmedFinancialRecords = data
+          ))
+    },
+    async fetchPendingWithdrawalRecords(){
+      console.log("fetch withdrawal records");
+      await fetch("http://localhost:3003/api/org.acme.charity.FinancialRecord?filter=%7B%22where%22%3A%7B%22status%22%3A%22Pending%22%2C%22type%22%3A%22Withdrawal%22%7D%7D")
+      .then(response => response.json())
+      .then((data)=>(
+          this.pendingWithdrawalRecords = data
           ))
     },
 
-    async fetchBankStatements(){
+    async fetchPendingBankStatements(){
       console.log("fetch bank statements");
-      await fetch("http://localhost:3003/api/org.acme.charity.BankStatement")
+      await fetch("http://localhost:3003/api/org.acme.charity.BankStatement?filter=%7B%22where%22%3A%7B%22status%22%3A%22Pending%22%7D%7D")
       .then(response => response.json())
       .then((data)=>(
-          this.bankStatements = data
+          this.pendingBankStatements = data
           ))
     },
+    
+    async fetchConfirmedBankStatements(){
+      console.log("fetch bank statements");
+      await fetch("http://localhost:3003/api/org.acme.charity.BankStatement?filter=%7B%22where%22%3A%7B%22or%22%3A%5B%7B%22status%22%3A%22Successful%22%7D%2C%7B%22status%22%3A%22Denied%22%7D%5D%7D%7D")
+      .then(response => response.json())
+      .then((data)=>(
+          this.confirmedBankStatements = data
+          ))
+    },
+
     async fetchTreasury(){
       console.log("fetch treasury");
       await fetch("http://localhost:3003/api/org.acme.charity.Treasury")
@@ -176,11 +322,27 @@ export default {
     },
     mounted: function() {
         console.log("mounted");
-        this.fetchDonationRecords();
-        this.fetchWithdrawalRecords();
-        this.fetchBankStatements();
+        this.fetchPendingDonationRecords();
+        this.fetchConfirmedFinancialRecords();
+        this.fetchPendingWithdrawalRecords();
+        this.fetchPendingBankStatements();
+        this.fetchConfirmedBankStatements();
         this.fetchTreasury()
-    }
+    },
+    computed: {
+        filteredConfirmedDonationRecords: function () {
+        console.log(this.confirmedFinancialRecords)
+        return this.confirmedFinancialRecords.filter((row) => {
+            return row.type.match("Donation")
+            })
+        },
+        filteredConfirmedWithdrawalRecords: function () {
+        console.log(this.confirmedFinancialRecords)
+        return this.confirmedFinancialRecords.filter((row) => {
+            return row.type.match("Withdrawal")
+            })
+        }
+  }
 }
 </script>
 
